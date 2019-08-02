21eb614a4bd2c8a63e14db8eba1ed91a10917ffb
Artur Dolgii <artur.dolgii@gmail.com>
2019-08-02 23:11:47 +0300

    refactored todos-footer template

--- src/app/todos-footer/todos-footer.component.ts
+++ src/app/todos-footer/todos-footer.component.ts
@@ -4,23 +4,33 @@ import {TodosService} from "../todos-service";
 @Component({
     selector: "todos-footer",
     template: `
-        <small>
-            <div class="row">
-                <div class="col-md-3">
-                    <span>{{itemsLeft}} {{itemsLeft === 1 ? "item left" : "items left"}}</span>
-                </div>
-                <div class="col-md-6">
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="">All</a></li>
-                        <li class="list-inline-item"><a href="">Active</a></li>
-                        <li class="list-inline-item"><a href="">Completed</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3">
-                    <a href="">Clear completed</a>
-                </div>
-            </div>
-        </small>
+        <ul class="list-inline">
+            <li class="list-inline-item float-left">
+                <span class="btn">{{itemsLeft}} {{itemsLeft === 1 ? "item left" : "items left"}}</span>
+            </li>
+            <li class="list-inline-item">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <button type="button"
+                                class="btn btn-link">
+                            <span class="footer-text-small">All</span>
+                        </button>
+                    </li>
+                    <li class="list-inline-item">
+                        <button type="button"
+                                class="btn btn-link">Active</button>
+                    </li>
+                    <li class="list-inline-item">
+                        <button type="button"
+                                class="btn btn-link">Completed</button>
+                    </li>
+                </ul>
+            </li>
+            <li class="list-inline-item float-right">
+                <button type="button"
+                        class="btn btn-link">Clear completed</button>
+            </li>
+        </ul>
     `,
     styles: []
 })
